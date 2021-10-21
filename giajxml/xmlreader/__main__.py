1c886b1cb87dbde46aabcc1be2a936d93fdbeff5
Sin Shimozono <una.veritas@me.com>
2021-10-22 01:47:48 +0900

    elementtree

--- giajxml/xmlreader/__main__.py
+++ giajxml/xmlreader/__main__.py
@@ -4,10 +4,29 @@ Created on 2021/10/20
 @author: Sin Shimozono
 '''
 
-import json
+#import json
 import xmltodict
 #from lxml.etree import parse
 from collections import OrderedDict
+from lxml import etree
+
+with open('../GIAJ_DL/FG-GML-503032-ALL-20210701/test-0001.xml') as fp:
+#with open('test.xml') as fp:
+    xtree = etree.parse(fp)
+
+#xml = bytes(bytearray(text, encoding='utf-8'))
+#tree = etree.parse(xml)
+root = xtree.getroot()
+
+print( 'root tag = %s' % root.tag )
+print( 'root nsmap = %s' % root.nsmap )
+#print( etree.tostring(xtree, encoding='utf-8').decode())
+
+for elem in xtree.iter():
+    print('tag={}, attr={}, text={}'.format(elem.tag, elem.get('att'), elem.text))
+
+print(xtree.find('gml:posList', namespaces={'gml': 'http://www.opengis.net/gml/3.2'}) )
+exit()
 
 def traverse(dic, search):
     #print(search[0:])
@@ -25,10 +44,7 @@ def traverse(dic, search):
 
     
 if __name__ == '__main__':
-    # XMLÉtÉ@ÉCÉãÇÃèàóù
     with open('../GIAJ_DL/FG-GML-503032-ALL-20210701/FG-GML-503032-AdmArea-20210701-0001.xml', encoding='utf-8') as fp:
-        # xmlì«Ç›çûÇ›
-        # xml Å® dict
         oddata = xmltodict.parse(fp.read())
     
     #print(dict_data)
