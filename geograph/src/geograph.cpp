f6b0f242ba48c9533fe1cc8d94238964b57b3c88
Sin Shimozono <una.veritas@me.com>
2022-03-22 01:05:29 +0900

    geohash

--- geograph/src/geograph.cpp
+++ geograph/src/geograph.cpp
@@ -14,8 +14,9 @@
 #include <iomanip>
 #include <algorithm>
 
-
 using namespace std;
+
+#include "geohash.h"
 
 vector<string> split(string& input, char delimiter) {
     istringstream stream(input);
@@ -30,20 +31,28 @@ vector<string> split(string& input, char delimiter) {
 struct node_edge {
 	unsigned long id;
 	double lat, lon;
+	string hash;
 	vector<unsigned long> adjacents;
+
+	static constexpr int prec = 8;
+
+	node_edge(void) : id(0), lat(0), lon(0), hash(""), adjacents({0}) { }
+//	node_edge(const char * key) : id(0), lat(0), lon(0), hash(key), adjacents({0}) { }
 
 	node_edge(const vector<string> & strvec) {
 		id = strtol(strvec[0].c_str(), NULL, 10);
 		lat = strtod(strvec[1].c_str(), NULL);
 		lon = strtod(strvec[2].c_str(), NULL);
+		hash = geohash::encode(lat, lon, prec);
 		for(unsigned i = 3; i < strvec.size(); ++i) {
 			adjacents.push_back(strtol(strvec[i].c_str(), NULL, 10));
 		}
 	}
 
 	friend ostream & operator<<(ostream & out, const node_edge & ne) {
-		out << ne.id << " (" << setprecision(10) << ne.lat << ","
-				<< setprecision(10) << ne.lon << "), ";
+		out << setw(10) << ne.id << " " << ne.hash << " "
+				<< " (" << fixed << setprecision(7) << ne.lat << ","
+				<< setprecision(7) << ne.lon << "), ";
 		out << "{";
 		for(unsigned int i = 0; i+1 < ne.adjacents.size(); ++i) {
 			out << ne.adjacents[i] << ", ";
@@ -51,131 +60,9 @@ struct node_edge {
 		out << ne.adjacents[ne.adjacents.size() -1] << "} ";
 		return out;
 	}
+
+
 };
-
-struct geohash {
-	static constexpr double MAX_LAT = 90.0;
-	static constexpr double MIN_LAT = -90.0;
-	static constexpr double MAX_LONG = 180.0;
-	static constexpr double MIN_LONG = -180.0;
-	static constexpr char char_map[] = "0123456789bcdefghjkmnpqrstuvwxyz";
-	static constexpr int bz_map[] = {
-			10, 11, 12, 13, 14, 15, 16, -1,
-			17, 18, -1,
-			19, 20, -1,
-			21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-	};
-
-	struct interval {
-		double high, low;
-
-		interval(const double &hi, const double &lo) :
-				high(hi), low(lo) {
-		}
-	};
-
-	static int char_revmap(const char c) {
-		int result = -1;
-		if (c < '9' + 1) {
-			//"0123456789"
-			result = c - '0';
-		} else if (c < 'z' + 1) {
-			result = bz_map[c - 'b'];
-		}
-		if (result < 0)
-			return -1;
-		return result;
-	}
-
-	static string encode(double lat, double lng, int precision = 8) {
-		static char hash[12];
-		if (precision < 1 || precision > 12)
-			precision = 6;
-		if (lat <= 90.0 && lat >= -90.0 && lng <= 180.0 && lng >= -180.0) {
-			hash[precision] = '\0';
-
-			precision *= 5.0;
-
-			interval lat_interval(MAX_LAT, MIN_LAT);
-			interval lng_interval(geohash::MAX_LONG, MIN_LONG);
-
-			interval *intv;
-			double coord, mid;
-			int is_even = 1;
-			unsigned int hashChar = 0;
-			int i;
-			for (i = 1; i <= precision; i++) {
-				if (is_even) {
-					intv = &lng_interval;
-					coord = lng;
-				} else {
-					intv = &lat_interval;
-					coord = lat;
-				}
-				mid = (intv->low + intv->high) / 2.0;
-				hashChar = hashChar << 1;
-				if (coord > mid) {
-					intv->low = mid;
-					hashChar |= 0x01;
-				} else
-					intv->high = mid;
-
-				if (!(i % 5)) {
-					hash[(i - 1) / 5] = char_map[hashChar];
-					hashChar = 0;
-				}
-				is_even = !is_even;
-			}
-		} else {
-			hash[0] = '\0';
-		}
-		return string(hash);
-	}
-
-	struct coordbox {
-	    double n;
-	    double e;
-	    double s;
-	    double w;
-	};
-
-	static coordbox decode(const string & hash) {
-		coordbox box = { 0.0, 0.0, 0.0, 0.0 };
-
-		if (hash.length() > 0) {
-			unsigned int char_mapIndex;
-			interval lat_interval = { MAX_LAT, MIN_LAT };
-			interval lng_interval = { MAX_LONG, MIN_LONG };
-			interval *intv;
-
-			int is_even = 1;
-			double delta;
-			unsigned int i, j;
-			for (i = 0; i < hash.length(); i++) {
-				char_mapIndex = geohash::char_revmap(hash[i]);
-				if (char_mapIndex < 0)
-					break;
-				// Interpret the last 5 bits of the integer
-				for (j = 0; j < 5; j++) {
-					intv = is_even ? &lng_interval : &lat_interval;
-					delta = (intv->high - intv->low) / 2.0;
-					if ((char_mapIndex << j) & 0x0010)
-						intv->low += delta;
-					else
-						intv->high -= delta;
-
-					is_even = !is_even;
-				}
-			}
-			box.n = lat_interval.high;
-			box.e = lng_interval.high;
-			box.s = lat_interval.low;
-			box.w = lng_interval.low;
-		}
-		return box;
-	}
-};
-
 
 int main(const int argc, const char * argv[]) {
 	ifstream csvf;
@@ -198,23 +85,35 @@ int main(const int argc, const char * argv[]) {
         	cerr << "insufficient parameters for a node_edge." << endl;
         	continue;
         }
-        ggraph.push_back(node_edge(strvec));
+        node_edge a_node(strvec);
+        ggraph.push_back(a_node);
     }
 
+    sort(ggraph.begin(), ggraph.end(),
+    		[](const node_edge & a, const node_edge & b) { return a.hash < b.hash; }
+    		);
     unsigned degree = 0;
+    unsigned ix = 0;
     cout << ggraph.size() << " nodes." << endl;
     for(unsigned int i = 0; i < ggraph.size(); ++i) {
-    	if (i < min((unsigned) 12, (const unsigned) ggraph.size()) ) {
-    		cout << geohash::encode(ggraph[i].lat, ggraph[i].lon, 9) << " ";
+    	if (i < min((unsigned) 8, (const unsigned) ggraph.size()) ) {
     		cout << ggraph[i] << endl;
     	}
     	if (degree < ggraph[i].adjacents.size()) {
     		degree = ggraph[i].adjacents.size();
+    		ix = i;
     	}
     }
-    cout << "max. degree is " << degree << "." << endl;
-    geohash::coordbox coord = geohash::decode("wvuxn178z");
-    cout << coord.s << ", " << coord.n << ", " << coord.w << ", " << coord.e << endl;
-    cout << coord.n - (coord.n - coord.s)/2 << ", " << coord.e - (coord.e - coord.w)/2 << endl;
+    cout << "max. degree is " << degree << " at " << ggraph[ix].hash << endl;
+
+    node_edge key;
+    key.hash = "wvuwzv9";
+    vector<node_edge>::iterator lb = lower_bound(ggraph.begin(), ggraph.end(),
+    		key,
+    		[](const node_edge & a, const node_edge &b){ return a.hash < b.hash; });
+    for(auto it = lb; it != ggraph.end() && ((it->hash).substr(0, key.hash.length()) == key.hash); ++it) {
+  		cout << *it << endl;
+    }
+
     return EXIT_SUCCESS;
 }
--- geograph/src/geohash.h
+++ geograph/src/geohash.h
@@ -1,77 +1,157 @@
- /*
- *  geohash.h
- *  libgeohash
- *
- *  Created by Derek Smith on 10/6/09.
- *  Copyright (c) 2010, SimpleGeo
- *	All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without 
- *  modification, are permitted provided that the following conditions are met:
-
- *  Redistributions of source code must retain the above copyright notice, this list
- *  of conditions and the following disclaimer. Redistributions in binary form must 
- *  reproduce the above copyright notice, this list of conditions and the following 
- *  disclaimer in the documentation and/or other materials provided with the distribution.
- *  Neither the name of the SimpleGeo nor the names of its contributors may be used
- *  to endorse or promote products derived from this software without specific prior 
- *  written permission. 
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
- *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
- *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
- *  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
- *  OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-// Metric in meters
-typedef struct GeoBoxDimensionStruct {
-	
-	double height;
-	double width;
-
-} GeoBoxDimension;
-
-typedef struct GeoCoordStruct {
-    
-    double latitude;
-    double longitude;
-    
-    double north;
-    double east;
-    double south;
-    double west;
-
-	GeoBoxDimension dimension;
-    
-} GeoCoord;
-
 /*
- * Creates a the hash at the specified precision. If precision is set to 0.
- * or less than it defaults to 12.
- */
-extern char* geohash_encode(double lat, double lng, int precision);
-
-/* 
- * Returns the latitude and longitude used to create the hash along with
- * the bounding box for the encoded coordinate.
- */
-extern GeoCoord geohash_decode(char* hash);
-
-/* 
- * Return an array of geohashes that represent the neighbors of the passed
- * in value. The neighbors are indexed as followed:
+ * geohash.h
  *
- *                  N, NE, E, SE, S, SW, W, NW
- * 					0, 1,  2,  3, 4,  5, 6, 7
- */ 
-extern char** geohash_neighbors(char* hash);
-
-/*
- * Returns the width and height of a precision value.
+ *  Created on: 2022/03/21
+ *      Author: Sin Shimozono
  */
-extern GeoBoxDimension geohash_dimensions_for_precision(int precision);
+
+#ifndef GEOHASH_H_
+#define GEOHASH_H_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct geohash {
+private:
+	static constexpr double MAX_LAT = 90.0;
+	static constexpr double MIN_LAT = -90.0;
+	static constexpr double MAX_LONG = 180.0;
+	static constexpr double MIN_LONG = -180.0;
+	static constexpr char char_map[] = "0123456789bcdefghjkmnpqrstuvwxyz";
+	static constexpr int bz_map[] = {
+			10, 11, 12, 13, 14, 15, 16, -1,
+			17, 18, -1,
+			19, 20, -1,
+			21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+	};
+
+	static int char_revmap(const char c) {
+		int result = -1;
+		if (c < '9' + 1) {
+			//"0123456789"
+			result = c - '0';
+		} else if (c < 'z' + 1) {
+			result = bz_map[c - 'b'];
+		}
+		if (result < 0)
+			return -1;
+		return result;
+	}
+
+public:
+
+	struct interval {
+		double high, low;
+
+		interval(const double &hi, const double &lo) :
+				high(hi), low(lo) {
+		}
+	};
+
+	struct coordbox {
+	    double n;
+	    double e;
+	    double s;
+	    double w;
+
+	    double center_lat() const {
+	    	return n - (n-s)/2.0;
+	    }
+
+	    double center_lon() const {
+	    	return e - (e-w)/2.0;
+	    }
+
+	    friend ostream & operator<<(ostream & out, const coordbox & box) {
+	    	out << "(s: " << box.s << ", n: " << box.n
+	    			<< ", w: " << box.w << ", e: " << box.e << ") ";
+	    	return out;
+	    }
+	};
+
+	static string encode(const double & lat, const double & lng, int precision = 8) {
+		static char hash[12];
+		if (precision < 1 || precision > 12)
+			precision = 6;
+		if (lat <= 90.0 && lat >= -90.0 && lng <= 180.0 && lng >= -180.0) {
+			hash[precision] = '\0';
+
+			precision *= 5.0;
+
+			interval lat_interval(MAX_LAT, MIN_LAT);
+			interval lng_interval(geohash::MAX_LONG, MIN_LONG);
+
+			interval *intv;
+			double coord, mid;
+			int is_even = 1;
+			unsigned int hashChar = 0;
+			int i;
+			for (i = 1; i <= precision; i++) {
+				if (is_even) {
+					intv = &lng_interval;
+					coord = lng;
+				} else {
+					intv = &lat_interval;
+					coord = lat;
+				}
+				mid = (intv->low + intv->high) / 2.0;
+				hashChar = hashChar << 1;
+				if (coord > mid) {
+					intv->low = mid;
+					hashChar |= 0x01;
+				} else
+					intv->high = mid;
+
+				if (!(i % 5)) {
+					hash[(i - 1) / 5] = char_map[hashChar];
+					hashChar = 0;
+				}
+				is_even = !is_even;
+			}
+		} else {
+			hash[0] = '\0';
+		}
+		return string(hash);
+	}
+
+	static coordbox decode(const string & hash) {
+		coordbox box = { 0.0, 0.0, 0.0, 0.0 };
+
+		if (hash.length() > 0) {
+			unsigned int char_mapIndex;
+			interval lat_interval = { MAX_LAT, MIN_LAT };
+			interval lng_interval = { MAX_LONG, MIN_LONG };
+			interval *intv;
+
+			int is_even = 1;
+			double delta;
+			unsigned int i, j;
+			for (i = 0; i < hash.length(); i++) {
+				char_mapIndex = geohash::char_revmap(hash[i]);
+				if (char_mapIndex < 0)
+					break;
+				// Interpret the last 5 bits of the integer
+				for (j = 0; j < 5; j++) {
+					intv = is_even ? &lng_interval : &lat_interval;
+					delta = (intv->high - intv->low) / 2.0;
+					if ((char_mapIndex << j) & 0x0010)
+						intv->low += delta;
+					else
+						intv->high -= delta;
+
+					is_even = !is_even;
+				}
+			}
+			box.n = lat_interval.high;
+			box.e = lng_interval.high;
+			box.s = lat_interval.low;
+			box.w = lng_interval.low;
+		}
+		return box;
+	}
+
+};
+
+#endif /* GEOHASH_H_ */
