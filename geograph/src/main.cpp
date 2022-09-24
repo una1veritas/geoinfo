43b502e0605540a13b0d9bda073d1832ba4d55de
Sin Shimozono <una.veritas@me.com>
2022-09-24 23:38:39 +0900

    cartcoord

--- geograph/src/main.cpp
+++ geograph/src/main.cpp
@@ -30,100 +30,10 @@ using std::set;
 
 #include <cmath>
 #include "geodistance.h"
+#include "cartcoord.h"
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
-
-#define sign(x) ((x) > 0 ? 1 : ((x) == 0 ? 0: -1))
-
-struct CartCoord {
-	double x, y;
-
-	// origin
-	CartCoord() : x(0), y(0) {}
-
-	CartCoord(const double & xval, const double & yval) : x(xval), y(yval) {}
-
-	CartCoord(const geopoint & org, const geopoint & dst) {
-		x = org.distance_to(geopoint(org.lat, dst.lon));
-		y = org.distance_to(geopoint(dst.lat, org.lon));
-		if (dst.lon < org.lon)
-			x = -x;
-		if (dst.lat < org.lat)
-			y = -y;
-	}
-
-	static bool crossing(const CartCoord & a, const CartCoord & b, const CartCoord & c, const CartCoord & d) {
-		return (sign(a.outer_prod(b, c)) * sign(b.outer_prod(a,d)) > 0)
-				and (sign(c.outer_prod(d, a)) * sign(d.outer_prod(c,b)) > 0);
-	}
-
-	static double distance_between(const CartCoord & a, const CartCoord & b, const CartCoord & c, const CartCoord & d) {
-		if ( crossing(a,b,c,d) )
-			return 0;
-		return std::min(a.distance_to(c,d), b.distance_to(c,d));
-	}
-
-	static double length_along(CartCoord a, CartCoord b, CartCoord c, CartCoord d) {
-		CartCoord ab = b - a;
-		CartCoord cd = d - c;
-		double inprod_ab_cd = ab.x * cd.x + ab.y * cd.y;
-		if ( inprod_ab_cd < 0 ) { // swap c and d to make c -> d being same direction with a -> b
-			std::swap(c, d);
-			inprod_ab_cd = -inprod_ab_cd;
-		}
-		if (inprod_ab_cd == 0)
-			return 0;
-		if ( c.projection_on(a, b) < 0 and d.projection_on(a, b) < 0 ) {
-			// c and d are outside of a-b
-			return a.distance_to(b);
-		} else if ( c.projection_on(a, b) < 0 ) {
-			return std::min(inprod_ab_cd / a.distance_to(b) - c.projection_on(a, b), a.distance_to(b));
-		} else if ( d.projection_on(b, a) < 0 ) {
-			return std::min(inprod_ab_cd / a.distance_to(b) - d.projection_on(b, a), a.distance_to(b));
-		} else {
-			return inprod_ab_cd / a.distance_to(b);
-		}
-	}
-
-	static double cosine(const CartCoord & a, const CartCoord & b, const CartCoord & c, const CartCoord & d) {
-		return ((b.x - a.x) * (d.x - c.x) + (b.y - a.y) * (d.y - c.y)) / (a.distance_to(b) * c.distance_to(d));
-	}
-
-	CartCoord operator-(const CartCoord & b) const {
-		return CartCoord(b.x - x, b.y - y);
-	}
-
-	double distance_to(const CartCoord & b) const {
-		return sqrt((b.x - x)*(b.x - x) + (b.y - y)*(b.y - y));
-	}
-
-	double projection_on(const CartCoord & a, const CartCoord & b) const {
-		return ((x - a.x) * (b.x - a.x) + (y - a.y) * (b.y - a.y)) / a.distance_to(b);
-	}
-
-	// the norm of outer product around *this
-	double outer_prod(const CartCoord & a, const CartCoord & b) const {
-		double ax = a.x - x, ay = a.y - y;
-		double bx = b.x - x, by = b.y - y;
-		return  (ax * by) - (ay * bx);
-	}
-
-	double distance_to(const CartCoord & p, const CartCoord & q) const {
-		double dp = p.distance_to(*this);
-		double dq = q.distance_to(*this);
-		if ( this->projection_on(p, q) < 0 or this->projection_on(q, p) < 0 ) {
-			// outer of both p and q
-			return std::min(dp, dq);
-		}
-		return abs(p.outer_prod(q, *this))/p.distance_to(q);
-	}
-
-	friend std::ostream & operator<<(std::ostream & out, const CartCoord & v) {
-		out << "(" << std::setw(3) << v.x << ", " << v.y << ") ";
-		return out;
-	}
-};
 
 vector<string> split(string& input, char delimiter) {
     istringstream stream(input);
@@ -234,9 +144,9 @@ int main(int argc, char * argv[]) {
 				const uint64_t & a_id = a.id();
 				pa = CartCoord(curr, a.point());
 				pb = CartCoord(curr, b.point());
-				if (  CartCoord::cosine(pc, pn, pa, pb) > 0.7 and
-						CartCoord::length_along(pc, pn, pa, pb) / pa.distance_to(pb) >= 0.3 and
-						CartCoord::distance_between(pc, pn, pa, pb) <= delta ) {
+				if ( CartCoord::cosine(pc, pn, pa, pb) > 0.7
+						and CartCoord::length_along(pc, pn, pa, pb) / pa.distance_to(pb) >= 0.5
+						and CartCoord::distance_between(pc, pn, pa, pb) <= delta ) {
 					if (a_id < b_id)
 						edgeseq[i].insert(pair<uint64_t,uint64_t>(a_id, b_id));
 					else
@@ -273,7 +183,7 @@ struct GeoRect {
 		for(const auto & p : vec) {
 			east = std::min(east, p.lon);
 			west = std::max(west, p.lon);
-			south = std::min(south , p.lat);
+			south = std::min(south, p.lat);
 			north = std::max(north, p.lat);
 		}
 	}
@@ -328,8 +238,11 @@ struct GeoRect {
 };
 
 int show_in_sdl_window(const geograph & map, const std::vector<geopoint> & track, const std::vector<std::set<std::pair<uint64_t,uint64_t>>> & roadsegs) {
+	constexpr unsigned int WINDOW_MIN_WIDTH = 1024;
+	constexpr unsigned int WINDOW_MIN_HEIGHT = 768;
 	SDL_Window * window = NULL;
 	SDL_Renderer * renderer = NULL;
+
 	union Color {
 		struct {
 			uint8_t r;
@@ -347,24 +260,21 @@ int show_in_sdl_window(const geograph & map, const std::vector<geopoint> & track
 		}
 	} c, c2;
 
-	constexpr unsigned int WINDOWSIZE = 1024;
-	int winheight = WINDOWSIZE, winwidth = WINDOWSIZE;
-	GeoRect traj_area(track);
-	GeoRect viewarea = { 0,0,0,0 };
+	struct {
+		unsigned int w, h;
+	} winrect = {WINDOW_MIN_WIDTH, WINDOW_MIN_HEIGHT};
 
+	GeoRect traj_area(track);
 	//cout << traj_area.width_meter() << ", " << traj_area.height_meter() << endl;
 	double aspect = traj_area.width_meter() / traj_area.height_meter();
 	// pixel per degree
-	double vscale = double(WINDOWSIZE) / (traj_area.north - traj_area.south);
-	double hscale = double(WINDOWSIZE) * aspect / (traj_area.west - traj_area.east);
-	viewarea(traj_area.north,
-			traj_area.east,
-			traj_area.north - double(winheight) / vscale,
-			traj_area.east + double(winwidth) / hscale);
+	double vscale = double(WINDOW_MIN_HEIGHT) / (traj_area.north - traj_area.south);
+	double hscale = double(WINDOW_MIN_HEIGHT) * aspect / (traj_area.west - traj_area.east);
+	GeoRect viewarea(traj_area);
 	cout << "hscale = " << hscale << ", vscale = " << vscale << endl;
 	if ( (SDL_Init( SDL_INIT_VIDEO ) < 0)
 			or !(window = SDL_CreateWindow( "Geograph", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-					winwidth, winheight, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE))
+					winrect.w, winrect.h, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE))
 			or !(renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE)) ) {
 		cerr << "Error: " << SDL_GetError() << endl;
 		return EXIT_FAILURE;
@@ -424,15 +334,15 @@ int show_in_sdl_window(const geograph & map, const std::vector<geopoint> & track
 					// after the succeeding resize.
 					switch (event.window.event) {
 					case SDL_WINDOWEVENT_SIZE_CHANGED:
-						winwidth = event.window.data1;
-						winheight = event.window.data2;
+						winrect.w = event.window.data1;
+						winrect.h = event.window.data2;
 						//cout << "CHANGED " << winrect.h << ", " << winrect.w << endl;
 						//break;
 					//case SDL_WINDOWEVENT_RESIZED:
 						update = true;
 						//cout << "RESIZED" << endl;
-						viewarea.south = viewarea.north - double(winheight) / vscale;
-						viewarea.west = viewarea.east + double(winwidth) / hscale;
+						viewarea.south = viewarea.north - double(winrect.h) / vscale;
+						viewarea.west = viewarea.east + double(winrect.w) / hscale;
 						break;
 					}
 				break;
