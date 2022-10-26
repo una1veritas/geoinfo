a1e75e03826d416324bd19d19e48c8d0c87e30d4
Sin Shimozono <una.veritas@icloud.com>
2022-10-26 17:45:13 +0900

    sample

---
+++ geograph_sample/src/geograph_sample.cpp
@@ -0,0 +1,77 @@
+//============================================================================
+// Name        : geograph_sample.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <iomanip>
+#include <map>
+
+#include "geograph.h"
+#include "bgeohash.h"
+
+using namespace std;
+
+
+vector<string> split(string& input, char delimiter) {
+    istringstream stream(input);
+    string field;
+    vector<string> result;
+    while (getline(stream, field, delimiter)) {
+        result.push_back(field);
+    }
+    return result;
+}
+
+int main(int argc, char * argv[]) {
+	ifstream csvf;
+
+	if (argc != 3) {
+		cerr << "usage: command [geograph csv file name]" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	cout << "reading geograph file " << argv[1] << ". " << endl;
+	csvf.open(argv[1]);
+	if (! csvf ) {
+		cerr << "open a geograph file " << argv[1] << " failed." << endl;
+		exit(EXIT_FAILURE);
+	}
+	geograph ggraph;
+    string line;
+    while (getline(csvf, line)) {
+        vector<string> strvec = split(line, ',');
+        if (strvec.size() < 4) {
+        	cerr << "insufficient parameters to define a node_edge." << endl;
+        	continue;
+        }
+		uint64_t id = stoull(strvec[0]);
+		double lat = stod(strvec[1]);
+		double lon = stod(strvec[2]);
+        std::vector<uint64_t> adjacents;
+        for(unsigned int i = 3; i < strvec.size(); ++i)
+        	adjacents.push_back(stoull(strvec[i]));
+        ggraph.insert(id, lat, lon, adjacents);
+        adjacents.clear();
+    }
+    csvf.close();
+
+    cout << "goegraph node size = " << std::dec << ggraph.size() << endl;
+
+	// show some entries.
+    int count = 0;
+    for(const auto & a_pair : ggraph.nodemap()) {
+    	cout << a_pair.second << endl;
+    	count += 1;
+    	if (count > 100) {
+    		cout << "..." << endl;
+    		break;
+    	}
+    }
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
