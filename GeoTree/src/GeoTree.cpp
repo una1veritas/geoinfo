e69ac49f816d68d58e809671f023d9caeb08cf9b
Sin Shimozono <una.veritas@icloud.com>
2022-11-29 19:18:41 +0900

    GeoTree

--- GeoTree/src/GeoTree.cpp
+++ GeoTree/src/GeoTree.cpp
@@ -9,37 +9,50 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-
+#include <set>
 #include "bgeohash.h"
 #include "geograph.h"
 
 using namespace std;
 
 //ノードのデータ構造を定義
-struct GeoNode{
-	int depth；
-	static constexpr char * char_map_geohash = (char *) "0123456789bcdefghjkmnpqrstuvwxyz";
-	static constexpr char * char_map_16 = (char *) "0123456789ABCDEF";
+struct GeoNode {
+private:
+	bgeohash geohash;  	// 深さ，およびこのノードが対応する領域，geohash の精度が深さを表す
+	enum {
+		TYPE_SETNODE = 0,
+		TYPE_DIVIDERNODE,
+	} node_type;
 
-	bool leaf {}；
+	union {
+		struct {
+			double threshold;  	// 分割する緯度または経度, どちらかは geohash の精度が偶数（経度）か奇数（緯度）で判別
+			GeoNode * left, * right;
+		};
+		struct {
+			set<geopoint> * points;
+		};
+	};
 
-	//内部ノードの場合
-	bool lon {};
-	//int 区切る緯度または経度の値
-	int lon = 0;
+public:
+	GeoNode(const bgeohash & ghash, const vector<geopoint> & pointseq) {
+		// 葉の場合のコンストラクタ．
+		// 点は vector から set に移すだけ．
+		geohash = bgeohash;
+		node_type = TYPE_SETNODE;
+		points = new set<geopoint>();
+		for(const auto & gp : pointseq) {
+			points->insert(gp);
+		}
+	}
 
-	GeoNode* left;
-	GeoNode* right;
-
-	//葉ノードの場合
-	set<uint64_t> points;
+	~GeoNode() {
+		if ( node_type == TYPE_SETNODE ) {
+			points->clear();
+			delete points;
+		}
+	}
 };
-/*
-//make_GeoTreeを完成させる
-make_GeoTree(点の集合,精度){
-	根ノード(=葉)を返す
-}*/
-
 
 int main(int argc, char * argv[]){
 
