c7482382088570927398d3f8c59868a7da598b72
una1veritas <una.vertas@icloud.com>
2023-07-15 23:38:40 +0900

    sgments reader

--- nakashima.k_gpspmatching/segment_reader/__main__.py
+++ nakashima.k_gpspmatching/segment_reader/__main__.py
@@ -3,6 +3,14 @@ import sys
 import re
 from _ast import If
 
+class geograph:
+    def __init__(self):
+        self.nodes = dict()
+        self.edges = dict()
+        
+    def __str__(self):
+        return str(list(self.nodes)[:10]) + ", " + str(list(self.edges.items())[:10])
+    
 trajectoryfile_name = '2019-10-05_114328.csv'
 segmentsfile_name = 'segments_data/2023-05-06_221836_segment.csv'
 edgefile_name = 'road_data/RoadNetwork_Fukuoka_Edge.csv'
@@ -36,24 +44,27 @@ with open(segmentsfile_name, 'r') as file:
             table.append(items)
 
 
-node_table = list()
-with open(nodefile_name, 'r') as file:
+graph = geograph()
+with open(nodefile_name, 'r', encoding='utf-8') as file:
     isheadline = True
     for a_line in file:
         items = a_line.strip().split(',')
         if isheadline :
-            node_table.append(items)
+            #node_table.append(items)
+            # ['node_id', 'lat', 'lon']
             isheadline = False
         else:
-            node_table.append([ eval(e) for e in items])
+            #node_table.append([ eval(e) for e in items])
+            row = [ eval(e) for e in items]
+            graph.nodes[row[0]] = (row[1], row[2])
 
-edge_table = list()
 with open(edgefile_name, mode='r', encoding='utf-8') as file:
     isheadline = True
     for a_line in file:
         items = a_line.strip().split(',')
         if isheadline :
-            edge_table.append(items)
+            #edge_table.append(items)
+            # ['edge_id', 'start', 'end', 'length', 'linestring', 'highway', 'oneway', 'reversed', 'lanes', 'name']
             isheadline = False
         else:
             for i in range(len(items)):
@@ -67,7 +78,7 @@ with open(edgefile_name, mode='r', encoding='utf-8') as file:
                     elif items[i].strip()[:10] == "LINESTRING" :
                         t = items[i].strip()[11:].replace(' ', ',')
                         items[i] = eval(t)
-            edge_table.append(items)
+            graph.edges[(items[1], items[2])] = items[3:]
 
 print('---------segments-----------')
 print(table[0])
@@ -75,14 +86,5 @@ print('--------------------')
 for e in table[1:10]:
     print(e)
 
-print('---------nodes-----------')
-print(node_table[0])
-print('--------------------')
-for e in node_table[1:10]:
-    print(e)
-
-print('---------edges-----------')
-print(edge_table[0])
-print('--------------------')
-for e in edge_table[1:10]:
-    print(e)
+print('---------graph-----------')
+print(graph)
