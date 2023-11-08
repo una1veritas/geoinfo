351c0a4c6c265f57dc85d0875bd37df6b6118b2d
Sin Shimozono <una.veritas@me.com>
2023-11-09 02:18:16 +0900

    Graph

--- geograph_reader/src/geograph_reader.cpp
+++ geograph_reader/src/geograph_reader.cpp
@@ -17,10 +17,12 @@
 using namespace std;
 
 struct Graph {
+private:
 	set<unsigned long> nodes;
 	map<unsigned long, pair<double, double> > node_coordinate;
 	map<unsigned long, vector<unsigned long> > node_adjacents;
 
+public:
 	// make as an empty graph
 	Graph() {}
 
@@ -31,6 +33,48 @@ struct Graph {
 		for(auto & nodelistpair : node_adjacents) {
 			nodelistpair.second.clear();
 		}
+	}
+
+	unsigned long size() const {
+		return nodes.size();
+	}
+
+	// insert/register a new node.
+	void insert_node(const unsigned long & id, const double & lat, const double & lon) {
+		nodes.insert(id);
+        node_coordinate[id] = pair<double,double>(lat,lon);
+        node_adjacents[id] = vector<unsigned long>();
+	}
+
+	// add an edge. *** an adjacent node can be added more than once! ***
+	void add_edge(const unsigned long & id, const unsigned long & another) {
+		if ( id < another ) {
+			if ( ! node_adjacents.contains(id) ) {
+				node_adjacents[id] = vector<unsigned long>();
+			}
+        	node_adjacents[id].push_back(another);
+		} else {
+			if ( ! node_adjacents.contains(another) ) {
+				node_adjacents[another] = vector<unsigned long>();
+			}
+        	node_adjacents[another].push_back(id);
+		}
+	}
+
+	double latitude(const unsigned long & id) {
+		return node_coordinate[id].first;
+	}
+
+	double longitude(const unsigned long & id) {
+		return node_coordinate[id].second;
+	}
+
+	const set<unsigned long> & node_ids() const {
+		return nodes;
+	}
+
+	const vector<unsigned long> & adjacents(const unsigned long & id) const {
+		return node_adjacents[id];
 	}
 };
 
@@ -47,14 +91,15 @@ vector<string> split(string& input, char delimiter) {
 }
 
 int main(const int argc, const char * argv[]) {
+	ifstream csvfile;
+
 	if (! (argc > 0) ) {
 		cerr << "give an input file name." << endl;
 		exit(EXIT_FAILURE); // terminates with error.
 	}
 	cout << "input file name = " << argv[1] << endl;
 
-	ifstream csvfile(argv[1]);
-
+	csvfile.open(argv[1]);
 	if (! csvfile ) {
 		cerr << "opening the file " << argv[1] << " failed." << endl;
 		exit(EXIT_FAILURE);
@@ -75,11 +120,9 @@ int main(const int argc, const char * argv[]) {
 		unsigned long id = stoull(strvec[0]);
 		double lat = stod(strvec[1]);
 		double lon = stod(strvec[2]);
-        graph.nodes.insert(id);
-        graph.node_coordinate[id] = pair<double,double>(lat,lon);
-        graph.node_adjacents[id] = vector<unsigned long>();
+        graph.insert_node(id, lat, lon);
         for(unsigned int i = 3; i < strvec.size(); ++i)
-        	graph.node_adjacents[id].push_back(stoull(strvec[i]));
+        	graph.add_edge(id, stoull(strvec[i]));
     }
     cout << count << "lines finished." << endl;
     csvfile.close();
@@ -87,25 +130,27 @@ int main(const int argc, const char * argv[]) {
 
     cout << "node id (lattitude, longitude):" << endl;
     count = 0;
-    for(const auto & an_id : graph.nodes) {
-    	cout << an_id << " (" << graph.node_coordinate[an_id].first << ", "
-    			<< graph.node_coordinate[an_id].second << "), " ;
+    for(const auto & an_id : graph.node_ids() ) {
+    	cout << an_id << " (" << graph.latitude(an_id) << ", "
+    			<< graph.longitude(an_id) << "), " ;
     	if (++count > 100)
     		break;
     }
     cout << endl;
-    cout << graph.nodes.size() << " nodes." << endl << endl;
+    cout << graph.size() << " nodes." << endl << endl;
 
     cout << endl << "edges: " << endl;
     count = 0;
-    for(const auto & a_pair : graph.node_adjacents) {
-    	if (count < 100) {
-			for(const auto & endpoint : a_pair.second) {
-				cout << "(" << a_pair.first << ", " << endpoint << "), ";
+    for(const auto & id : graph.node_ids() ) {
+    	for(const auto & another : graph.adjacents(id) ) {
+			if (count < 100) {
+				for(const auto & endpoint : a_pair.second) {
+					cout << "(" << a_pair.first << ", " << endpoint << "), ";
+				}
+				cout << endl;
 			}
-			cout << endl;
+			count += a_pair.second.size();
     	}
-    	count += a_pair.second.size();
     }
     cout << endl;
     cout << count << " edges." << endl;
