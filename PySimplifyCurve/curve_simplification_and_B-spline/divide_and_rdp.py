9e171d3be07b53eab03228bf9a20d602ae046fbd
Sin Shimozono <una.veritas@me.com>
2025-12-17 11:34:16 +0900

    3 partitioning

--- PySimplifyCurve/curve_simplification_and_B-spline/divide_and_rdp.py
+++ PySimplifyCurve/curve_simplification_and_B-spline/divide_and_rdp.py
@@ -42,12 +42,12 @@ def distance_to_line(p, a, b):
         return norm(bp)
     return abs(outer_prod_norm(ab, ap)/norm(ab))
 
-def min_max_distance_to_line(xypoints, startix, finalix, a, b):
+def distance_to_line_min_max_ix(xypoints, startix, stopix, a, b):
     min_dist = float('inf')
     max_dist = 0
     min_dist_ix = startix
     max_dist_ix = startix
-    for ix in range(startix, finalix + 1):
+    for ix in range(startix, stopix + 1):
         pt = xypoints[ix]
         dist = distance_to_line(pt, a, b)
         if dist < min_dist :
@@ -60,30 +60,41 @@ def min_max_distance_to_line(xypoints, startix, finalix, a, b):
     
 def simplify_RDP(xy : np.array, tolerance : float):
     mask = rdp.rdp(xy, epsilon=tolerance, return_mask=True)
-    xy_rdp = xy[mask]
+    xy_rdp = xy[mask]  #''' extracting elements and make a sub-sequence by boolean indexing'''
     return xy_rdp, [int(i) for i in np.where(mask)[0]]
 
 def simplify_divide_and_RDP(xy : np.array, tolerance : float):
     if len(xy) < 2 :
         raise ValueError('xy array must have two or more points.')
         exit(1)
-    curve_ixs = deque() # stack/queue of ix
-    curve_ixs.append(0)
-    curve_ixs.append(len(xy) - 1)
-    if curve_ixs[-1] - curve_ixs[0] < 1 :
-        '''no points between the curve start and the end.'''
-        simplified = deque(curve_ixs)
-        return simplified
+    '''準備'''
+    curve_ix_q = deque() # stack/queue of ix
+    curve_ix_q.append(0) # the first point
+    curve_ix_q.append(len(xy) - 1) # the last queue 
     
-    simplified = deque()
-    simplified.append(curve_ixs[0])
-    while len(curve_ixs) > 0 :
-        start_ix = curve_ixs.popleft()
-        final_ix= curve_ixs[0]
-        if final_ix - start_ix < 1 :
-            simplified.append(end_ix)
+    if curve_ix_q[-1] - curve_ix_q[0] < 1 :
+        '''no points between the indices start and stop.'''
+        return xy[[True, True]], [int(i) for i in np.where(mask)[0]]
+    
+    simplified_ix_q = deque()
+    simplified_ix_q.append(curve_ix_q[0]) 
+    '''単純化した区間の最初の点の添え字は、直前の区間の最後の点として追加済みとみなすため。'''
+    
+    '''decimates the indices of the points in xy 
+    by moving chosen indices of points from curve_ix_q to simplified_ix_q. '''
+    while len(curve_ix_q) > 0 :
+        '''base line (xy[start_ix], xy[stop_ix])'''
+        '''curve_ix_q は分割されうる区間の点の最初と最後のものの添え字を持つ．'''
+        start_ix = curve_ix_q.popleft() # get the first index and remove from queue
+        stop_ix= curve_ix_q[0] # the second index since the first one has been renoved
+
+        if stop_ix - start_ix == 1 :
+            '''間に点がないので無条件に追加｡'''
+            simplified.append(stop_ix)
             continue
-        '''check the first part'''
+        
+        '''三つの区間にわける．[start_ix, midletf_ix, midright_ix, stop_ix]'''
+        '''中央の区間は、開始と終了が同じ点でもかまわない。'''
         mid_start = math.ceil((final_ix - start_ix)/3)
         mid_final = math.floor((final_ix - start_ix)*2/3)
         (min_dist_ix, min_dist, max_dist_ix, max_dist) = min_max_distance_to_line(xy, start_ix, mid_start, xy[start_ix], xy[final_ix])
