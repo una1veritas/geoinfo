de91de079eae1998933a40e003fa254a05b9873d
Sin Shimozono <una.veritas@me.com>
2026-04-26 01:50:14 +0900

    ringarray

--- PointCurveDecimation/src/main.cpp
+++ PointCurveDecimation/src/main.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 int csv_reader(const std::string& filename, std::vector<Point2D> & tbl) {
     // 1. Open the CSV file
-    std::ifstream file("data.csv");
+    std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file!" << std::endl;
         return 1;
@@ -51,7 +51,33 @@ int csv_reader(const std::string& filename, std::vector<Point2D> & tbl) {
 
 int main() {
 	cout << "CSV reader!!" << endl; // prints !!!Hello World!!!
-	std::vector<Point2D> seqs = { {0, 0}, {1, 1}, {2, -1}, {2.0, -1.0}, {-4, -4}, {0, 0}, {1, 1}, {2, -2} };
+	std::vector<Point2D> seqs; // = { {0, 0}, {1, 1}, {2, -1}, {2.0, -1.5}, {-4, -4}, {0, 0}, {1, 1}, {2, -2} };
+	csv_reader("test.csv", seqs);
+	ringarray<Point2D> ring(4);
+	for (const auto & elem : seqs ) {
+		std::cout << elem << ", ";
+	}
+	std::cout << std::endl;
+	std::cout << "ring size = " << ring.size() << ", ring capacity = " << ring.capacity() << endl;
+	ring.push_back(seqs[0]);
+	ring.push_front(seqs[1]);
+	ring.push_back(seqs[2]);
+	ring.push_back(seqs[3]);
+	ring.pop_front();
+	ring.pop_front();
+	ring.push_back(seqs[0]);
+	ring.push_back(seqs[1]);
+	ring.push_back(seqs[2]);
+	ring.push_front(seqs[4]);
+	ring.push_back(seqs[5]);
+	ring.push_front(seqs[6]);
+	ring.push_back(seqs[7]);
+	ring.push_front(seqs[3]);
+	for (int i= 0; i < ring.size(); ++i) {
+		std::cout << ring[i] << std::endl;
+	}
+	std::cout << "ring size = " << ring.size() << ", ring capacity = " << ring.capacity() << endl;
+	/*
 	for (int i = 0; i + 2 < seqs.size() ; ++i) {
 		std::cout << seqs[i] << ", " << seqs[i+1] << ", " << seqs[i+2] << std::endl;
 		std::cout << " + " << seqs[i] + seqs[i+1] << std::endl;
@@ -65,7 +91,8 @@ int main() {
 		std::cout << " vector " << Point2D::vector(seqs[i], seqs[i+1]) << std::endl;
 		std::cout << " perpendicular vector "  << Point2D::perpvector(seqs[i], seqs[i+1]) << std::endl;
 	}
-
 	std::cout << Point2D(0, 0).rhombus(Point2D(1, 1), Point2D(0.8, 0.6)) << endl;
+	*/
+	
     return 0;
 }
--- PointCurveDecimation/src/ringarray.h
+++ PointCurveDecimation/src/ringarray.h
@@ -9,76 +9,93 @@
 #define RINGARRAY_H_
 
 #include <cstddef>
-#include <vector>
+#include <stdexcept>
 
 template <typename T>
-class RingArray {
-    std::vector<T> array;
-    size_t head = 0, tail = 0, count = 0;
+class ringarray {
+    T * array; 	// backbone array
+    std::size_t _capacity, _head = 0, _tail = 0, _count = 0;
 
 public:
-    RingArray(size_t capacity) : array(capacity) {}
+    ringarray(const size_t & capa) : _capacity(capa), _head(0), _tail(0), _count(0) {
+    	if (_capacity < 8)
+    		_capacity = 8;
+    	array = new T[_capacity];
+    }
 
-	~RingArray() = default;
+	~ringarray() {
+		delete [] array;
+		_capacity = 0;
+	}
 
 	size_t size() const {
-		return count;
+		return _count;
+	}
+
+	size_t capacity() const {
+		return _capacity;
 	}
 
 	bool is_empty() const {
-		return count == 0; // head == tail
+		return _count == 0; // head == tail
 	}
 
-    void push_back(T item) { // Enqueue
-    	if (! count < array.size())	<--- いや、capacity では？
-			resize(array.size()<<1); // Resize if full
-    	array[tail] = item;
-        tail = (tail + 1) % array.size();
-        count++;
+	// enqueue
+    void push_back(T item) {
+    	if ( ! (_count < _capacity) ) {
+    		resize(_capacity << 1); // Resize if full
+    	}
+    	array[_tail++] = item;
+    	_tail %= _capacity;
+        _count++;
     }
 
-    void push_front(T item) { // Enqueue
-    	if (! count < array.size())
-			resize(array.size()<<1); // Resize if full
-
-    	head = (head + array.size() - 1) % array.size();
-        array[head] = item;
-        count++;
+    void push_front(T item) {
+    	if (! (_count < _capacity) )
+			resize(_capacity<<1); // Resize if full
+    	_head = (_head + _capacity - 1) % _capacity;
+        array[_head] = item;
+        _count++;
     }
 
-    T pop_back() { // Enqueue
-		if (count == 0)
-			throw std::runtime_error("Buffer is empty");
-		tail = (tail + array.size() - 1) % array.size();
-		T item = array[tail];
-		count--;
+    T pop_back() { // dequeue
+//		if (count == 0)
+//			throw std::runtime_error("Buffer is empty");
+		_tail = (_tail + _capacity - 1) % _capacity;
+		T item = array[_tail];
+		_count--;
 		return item;
     }
 
-    T pop_front() { // Dequeue
-		if (count == 0)
-			throw std::runtime_error("Buffer is empty");
-		T item = array[head];
-		head = (head + 1) % array.size();
-		count--;
+    T pop_front() { // dequeue
+//		if (count == 0)
+//			throw std::runtime_error("Buffer is empty");
+		T item = array[_head];
+		_head = (_head + 1) % _capacity;
+		_count--;
 		return item;
 	}
 
-    T& operator[](size_t i) { // Random access
-        return array[(head + i) % array.size()];
+    // Random access to ix-th element from the head position
+    T& operator[](const size_t & ix) {
+    	if (ix > _count)
+    		throw std::out_of_range("Index is out of ringarray bounds");
+        return array[(_head + ix) % _capacity];
     }
 
-    void resize(size_t new_capacity) {
-    	if (new_capacity <= array.size())
+    void resize(size_t new_capa) {
+    	if (new_capa <= _capacity)
 			return; // No shrinking
-    	size_t old_capacity = array.size();
-		array.resize(new_capacity);
-		if (tail <= head) {
-			for (size_t ix = 0; ix < tail; ++ix) {
-				array[old_capacity + ix] = array[ix];
-			}
-			tail += old_capacity;
+    	T * new_array = new T[new_capa];
+    	size_t ix;
+		for (ix = 0; ix < _count; ++ix) {
+			new_array[ix] = array[(_head + ix) % _capacity];
 		}
+		delete [] array;
+		array = new_array;
+		_capacity = new_capa;
+		_head = 0;
+		_tail = ix;
 		return;
 	}
 };
