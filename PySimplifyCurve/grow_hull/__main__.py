9f445e60a1237fd081f9e3980e46a16da91f4834
Sin Shimozono <una.veritas@me.com>
2026-09-01 22:16:36 +0900

    removed obsolated rlines

--- PySimplifyCurve/grow_hull/__main__.py
+++ PySimplifyCurve/grow_hull/__main__.py
@@ -34,95 +34,6 @@ def simplify_RDP(xy : np.array, epsilon):
 
 
 
-# def delta_rect_decimation_alg(xy : list, delta, verbose = False, polygons = False) -> tuple:
-#     dixpath = list()        # decimated path, the seq. of indices to the reference point sequence xy
-#     polygon_seq = list()    # considered & finished polygons
-#     dixpath.add(0)       # the index of the the first point
-#     cvx = ConvexHull() 
-#     cvx_start_index = dixpath[-1];    # offset to index to xy
-#     cvx.add(xy[0])
-#     cvx.add(xy[1])
-#     cvx_diameter = distance(cvx.first_point(), cvx.last_point())
-#     ptix = 2
-#     while ptix < len(xy) :
-#         verbose and print(f'{ptix}, {xy[ptix]}, dia = {cvx_diameter}, delta = {delta}, {cvx}')
-#         verbose and print(f'side {cvx.polygon_index[-1]}-{cvx.polygon_index[0]}, {rhombus(cvx.polygon_point(-1), cvx.polygon_point(0), xy[ptix])} >= 0 or {cvx.polygon_index[1]}-{cvx.polygon_index[0]}, {rhombus(cvx.polygon_point(1), cvx.polygon_point(0), xy[ptix])} <= 0 ?' )
-#         if cvx_diameter <= delta and \
-#         ( rhombus(cvx.polygon_point(-1), cvx.polygon_point(0), xy[ptix]) >= 0 or rhombus(cvx.polygon_point(1), cvx.polygon_point(0), xy[ptix]) <= 0 ):
-#             verbose and print('within delta and in growth position')
-#             cvx.add(xy[ptix]) # should be succeeded
-#             cvx_diameter = max(cvx_diameter, distance(cvx[0], cvx[-1]) ) # = max(cvx_diameter, distance(cvx.first_point(), cvx.last_point()) )
-#             ptix += 1
-#             verbose and print(cvx)
-#             verbose and print()
-#             continue
-#
-#         verbose and print('check wether pt is furthest or not.')
-#         if cvx_diameter > distance(cvx[0], xy[ptix]) : # distance(cvx.first_point(), xy[ptix]) :
-#             verbose and print('getting nearer. stop extending cvx')
-#             cvx_lastix = cvx_start_index + cvx.size()
-#             dixpath.add(cvx_lastix)
-#             if polygons : 
-#                 polygon_seq.add([cvx_start_index + cvx.polygon_index[i] for i in range(len(cvx.polygon_index) + 1)])
-#             cvx.clear()
-#             # make new cvx for the 1st and 2nd points.
-#             cvx_start_index = cvx_lastix
-#             cvx.add(xy[cvx_lastix])
-#             cvx.add(xy[ptix])
-#             verbose and print(cvx)
-#             verbose and print(cvx[0], cvx[-1]) #print(cvx.first_point(), cvx.last_point())
-#             cvx_diameter = distance(cvx[0], cvx[-1])
-#             ptix += 1
-#             verbose and print(cvx)
-#             verbose and print()
-#             continue
-#
-#         # pt is in the growth position
-#         verbose and print('pt is at growth distance')
-#
-#         # preserve the outline of cvx before pt is added
-#         if polygons : 
-#             prevcvx_polygon = [cvx.polygon_point(i) for i in range(len(cvx.polygon_index) + 1)]
-#
-#         # add pt to test width
-#         cvx.add(xy[ptix])
-#         peakdists = cvx.peak_distances()
-#         verbose and print(peakdists, [e < delta for e in peakdists])
-#
-#         if all([e < delta for e in peakdists]) :
-#             cvx_diameter = distance(cvx[0], cvx[-1])
-#             ptix += 1
-#             verbose and print(cvx)
-#             verbose and print()
-#             continue
-#         else:
-#             prevcvx_lastix = len(cvx) - 2
-#             dixpath.add(cvx_start_index + prevcvx_lastix)
-#             if polygons : 
-#                 polygon_seq.add(prevcvx_polygon)
-#             cvx.clear()
-#             cvx_start_ix = prevcvx_lastix
-#             cvx.add(xy[prevcvx_lastix])
-#             cvx.add(xy[ptix])
-#             cvx_diameter = distance(cvx[0], cvx[-1])
-#             ptix += 1
-#             verbose and print(cvx)
-#             verbose and print()
-#             continue
-#         raise ValueError('???')
-#
-#     if len(cvx) > 0 :
-#         #print('points exhausted,', cvx)
-#         dixpath.add(cvx_start_index + len(cvx) - 1)
-#         if polygons : 
-#             polygon_seq.add([cvx.polygon_point(ix) for ix in range(len(cvx.polygon_index) + 1)])
-#
-#     if not polygons :
-#         return dixpath
-#     else:
-#         return (dixpath, polygon_seq)
-
-
 def Grow_Hull(xy : list, delta : float, record_polygons = False, verbose = False) -> tuple:
     decpath = list()        # index seq. of decimated point seq.
     polygons = list()    # considered & finished polygons
@@ -141,9 +52,9 @@ def Grow_Hull(xy : list, delta : float, record_polygons = False, verbose = False
             continue
         
         if cvx.add(xy[ix]) :
-            fw, bk, rt, lt = cvx.peak_distances()
-            if verbose : print(f'adding {xy[ix]} to cvx, peak distances = {fw}, {bk}, {rt}, {lt}')
-            if max(fw, bk, rt, lt ) > delta :
+            peak_dists = cvx.peak_distances()
+            if verbose : print(f'adding {xy[ix]} to cvx, peak distances = {peak_dists}')
+            if max(peak_dists) > delta :
                 if verbose : print(f'adding {xy[ix]} caused over size: \n')
                 # cancel the last addition
                 if verbose : print(len(cvx), f'start ix = {start_ix}', cvx)
@@ -185,7 +96,7 @@ def Grow_Hull(xy : list, delta : float, record_polygons = False, verbose = False
 
 if __name__ == '__main__':
     
-    run_info = { 'input': 'random', 'plot': True, 'annotate': False, 'runs': 10 }
+    run_info = { 'input': 'file', 'plot': True, 'annotate': False, 'runs': 10 }
     
     if run_info['input'] == 'specified' :
         delta = 0.5
@@ -325,8 +236,10 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots()
     ax.plot(x, y, 'y.-', lw=2.0, alpha=0.35)
-    #ax.plot(drx, dry, 'b.-', lw=1) #, alpha=0.75)
-    ax.plot(rdpx, rdpy, 'b.-', lw=1) #, alpha=0.75)
+    ax.plot(drx, dry, 'b.-', lw=1) #, alpha=0.75)
+    plt_title = f'Greedy+CH, delta = {delta}, points = {len(xy)}, simplified = {len(drseq)}'
+    #ax.plot(rdpx, rdpy, 'b.-', lw=1) #, alpha=0.75)
+    #plt_title = f'simplify_coords, delta = {delta}, points = {len(xy)}, simplified = {len(simplified)}'
     
     if len(polygons) > 0 :
         for polygon in polygons:
@@ -344,6 +257,6 @@ if __name__ == '__main__':
                 ha='center'     # Horizontal alignment of the text
             )
     plt.legend(['Input points', 'simplified path', 'polygon_index path'],loc='best')
-    plt.title('Point-curve simplification Test')
+    plt.title(plt_title)
     ax.set_aspect('equal')
     plt.show()
